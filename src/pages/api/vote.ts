534e7f6e4ded21fd1535afff1e61af0a594c50b3
abstdast <ayaanraoaaa@gmail.com>
2022-11-15 10:07:19 -0500

    new things

---
+++ src/pages/api/vote.ts
@@ -0,0 +1,62 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { object } from "zod";
+
+import { prisma } from "../../server/db/client";
+
+const vote = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const {id, choice} = req.body
+
+    if(choice > 2 || choice < 0) {
+
+        return res.status(401).json({"error": "choices out of range"})
+
+    }
+
+    const question = await prisma.question.findUnique({
+
+        where: {
+            id: id
+        }
+
+    });
+    
+    const answer = await prisma.answer.create({
+        data: {
+            questionId: id,
+            option: choice
+        }
+    })
+
+    const options1 = await prisma.answer.aggregate({
+        where: {
+            questionId: id,
+            option: 1
+        },
+
+        _count: {
+            option: true
+        }
+
+    })
+
+    const total = await prisma.answer.aggregate({
+
+        where: {
+            questionId: id, 
+        },
+
+        _count: {
+            option: true
+        }
+
+    })
+
+    const percentage = Math.round(options1._count.option/total._count.option * 100)
+
+    console.log(percentage)
+    res.status(200).json({"success": true, "percentage": percentage})
+
+};
+
+export default vote;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,39 +7,84 @@ import { useState } from "react";
 type Props = {
   questions: Question[];
 };
-
-const Home: NextPage = ({ questions }: Props) => {
+const Home = ({ questions }: Props) => {
   const [question, setQuestion] = useState<Question | null>(null);
+  const [result, setResult] = useState<number | null>(null);
+
+  const handleAnswer = async (choice: number) => {
+    const vote = await fetch("/api/vote", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: question?.id,
+        choice: choice,
+      }),
+    });
+
+    const data = await vote.json();
+    setResult(data.percentage);
+  };
 
   return (
     <>
       <h1 className="p-5 text-center text-4xl font-bold">Would You Rather</h1>
-      {question ? (
+      {question && !result ? (
         <div className="w-full">
           <h3 className="text-center text-3xl">{question.text}</h3>
           <div className="flex w-full p-5">
-            <div className="w-full rounded-l-lg bg-red-500 p-10 text-center">
+            <div
+              onClick={() => handleAnswer(1)}
+              className="w-full rounded-l-lg bg-red-500 p-10 text-center"
+            >
               {question.option1}
             </div>
-            <div className="w-full rounded-r-lg bg-blue-500 p-10 text-center">
+            <div
+              onClick={() => handleAnswer(2)}
+              className="w-full rounded-r-lg bg-blue-500 p-10 text-center"
+            >
               {question.option2}
             </div>
           </div>
         </div>
       ) : (
         <>
-          <div className="flex justify-center">
-            <button
-              onClick={() =>
-                questions.length > 0
-                  ? setQuestion(questions[0] as Question)
-                  : alert("no q")
-              }
-              className="text-center"
-            >
-              Start
-            </button>
-          </div>
+          {!result ? (
+            <div className="flex justify-center">
+              <button
+                onClick={() =>
+                  questions.length > 0
+                    ? setQuestion(questions[0] as Question)
+                    : alert("no q")
+                }
+                className="text-center"
+              >
+                Start
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="w-full">
+                <h3 className="text-center text-3xl">{question?.text}</h3>
+                <div className="flex w-full p-5">
+                  <div
+                    onClick={() => handleAnswer(1)}
+                    className="w-full rounded-l-lg bg-red-500 p-10 text-center"
+                  >
+                    {result}%
+                  </div>
+                  <div
+                    onClick={() => handleAnswer(2)}
+                    className="w-full rounded-r-lg bg-blue-500 p-10 text-center"
+                  >
+                    {100 - result}%
+                  </div>
+                </div>
+                <p onClick={}></p>
+              </div>
+            </>
+          )}
         </>
       )}
     </>
@@ -60,10 +105,9 @@ export const getServerSideProps = async () => {
 
 export default Home;
 
-
-
 /*
 
 () => setQuestion(questions[questions.indexOf(question) + 1])
+
 
 */
